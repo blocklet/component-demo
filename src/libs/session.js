0995649308be329be5fcf74277738093d878dd46
donotlb <donotlb@gmail.com>
2021-08-25 18:11:12 +0800

    chore: replace did-react with did-connect

--- src/libs/session.js
+++ src/libs/session.js
@@ -1,6 +1,6 @@
 /* eslint-disable object-curly-newline */
 import { useContext } from 'react';
-import { createAuthServiceSessionContext } from '@arcblock/did-react/lib/Session';
+import { createAuthServiceSessionContext } from '@arcblock/did-connect/lib/Session';
 
 const { SessionProvider, SessionContext, SessionConsumer, withSession } = createAuthServiceSessionContext();
 
