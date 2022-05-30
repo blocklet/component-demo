e2be2f2a6600558add6fddaae32a02bcfe862d5f
donotlb <donotlb@gmail.com>
2022-05-30 11:48:00 +0800

    chore: wrap app with mui theme provider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { ThemeProvider as MuiThemeProvider, StyledEngineProvider } from '@mui/material/styles';
+import { ThemeProvider } from 'styled-components';
+import { create } from '@arcblock/ux/lib/Theme';
 import { LocaleProvider } from '@arcblock/ux/lib/Locale/context';
 import { SessionProvider } from './libs/session';
 
@@ -12,10 +15,14 @@ if (window.blocklet && window.blocklet.prefix) {
 } else if (window.env && window.env.apiPrefix) {
   apiPrefix = window.env.apiPrefix;
 }
+const theme = create();
 
 function App() {
 
   return (
+    <StyledEngineProvider injectFirst>
+      <MuiThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
     <LocaleProvider translations={{}}>
       <SessionProvider
         serviceHost={apiPrefix}
@@ -24,6 +31,9 @@ function App() {
         <Main />
       </SessionProvider>
     </LocaleProvider>
+        </ThemeProvider>
+      </MuiThemeProvider>
+    </StyledEngineProvider>
   );
 }
 
