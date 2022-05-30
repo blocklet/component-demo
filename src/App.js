2f22165ada84e79075501ee11b01deba5f55bd4e
wangqi <donotlb@gmail.com>
2022-05-30 16:21:33 +0800

    chore: upgrade to react 18 and mui v5 (#26)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { ThemeProvider as MuiThemeProvider, StyledEngineProvider } from '@mui/material/styles';
+import { ThemeProvider } from 'styled-components';
+import { create } from '@arcblock/ux/lib/Theme';
 import { LocaleProvider } from '@arcblock/ux/lib/Locale/context';
 import { SessionProvider } from './libs/session';
 
-import CssBaseline from '@material-ui/core/CssBaseline';
+import CssBaseline from '@mui/material/CssBaseline';
 
 import Main from './page/main';
 
@@ -12,18 +15,23 @@ if (window.blocklet && window.blocklet.prefix) {
 } else if (window.env && window.env.apiPrefix) {
   apiPrefix = window.env.apiPrefix;
 }
+const theme = create();
 
 function App() {
 
   return (
-    <LocaleProvider translations={{}}>
-      <SessionProvider
-        serviceHost={apiPrefix}
-      >
-        <CssBaseline />
-        <Main />
-      </SessionProvider>
-    </LocaleProvider>
+    <StyledEngineProvider injectFirst>
+      <MuiThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
+          <LocaleProvider translations={{}}>
+            <SessionProvider serviceHost={apiPrefix}>
+              <CssBaseline />
+              <Main />
+            </SessionProvider>
+          </LocaleProvider>
+        </ThemeProvider>
+      </MuiThemeProvider>
+    </StyledEngineProvider>
   );
 }
 
