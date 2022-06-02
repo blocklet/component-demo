8a688518df061e1f9e2ca124bab207a43ac471fc
donotlb <donotlb@gmail.com>
2022-06-02 18:45:12 +0800

    chore: update blocklet-ui and adjust layout

--- src/page/main.js
+++ src/page/main.js
@@ -8,6 +8,7 @@ import TextsmsIcon from '@mui/icons-material/Textsms';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import IconButton from '@mui/material/IconButton';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 import ListItemText from '@mui/material/ListItemText';
 import { Alert } from '@mui/material';
 import Header from '@blocklet/ui-react/lib/Header';
@@ -21,33 +22,35 @@ export default function Main() {
   return (
     <Box display="flex" flexDirection="column" height="100vh" overflow="hidden">
       <Header />
-      <Box flex="1" my={4} overflow="auto">
+      <Box flex="1" py={4} overflow="auto">
         <Container>
-          <Alert severity="info">
-            Component Demo is a blocklet that is combined with other blocklets. <br />
-            These blocklets use a unified auth service and config. <br />
-            Click on "Discussion" or "Profile" to experience it!
-          </Alert>
-          <List>
-            <ListItemLink href="./discussion">
-              <ListItemIcon>
-                <TextsmsIcon />
-              </ListItemIcon>
-              <ListItemText primary="Discussion" />
-              <IconButton edge="end" aria-label="navigate" size="large">
-                <NavigateNextIcon />
-              </IconButton>
-            </ListItemLink>
-            <ListItemLink href="./profile">
-              <ListItemIcon edge="end">
-                <AccountBoxIcon />
-              </ListItemIcon>
-              <ListItemText primary="Profile" />
-              <IconButton edge="end" aria-label="navigate" size="large">
-                <NavigateNextIcon />
-              </IconButton>
-            </ListItemLink>
-          </List>
+          <MainContainer>
+            <Alert severity="info">
+              Component Demo is a blocklet that is combined with other blocklets. <br />
+              These blocklets use a unified auth service and config. <br />
+              Click on "Discussion" or "Profile" to experience it!
+            </Alert>
+            <List>
+              <ListItemLink href="./discussion">
+                <ListItemIcon>
+                  <TextsmsIcon />
+                </ListItemIcon>
+                <ListItemText primary="Discussion" />
+                <IconButton edge="end" aria-label="navigate" size="large">
+                  <NavigateNextIcon />
+                </IconButton>
+              </ListItemLink>
+              <ListItemLink href="./profile">
+                <ListItemIcon edge="end">
+                  <AccountBoxIcon />
+                </ListItemIcon>
+                <ListItemText primary="Profile" />
+                <IconButton edge="end" aria-label="navigate" size="large">
+                  <NavigateNextIcon />
+                </IconButton>
+              </ListItemLink>
+            </List>
+          </MainContainer>
         </Container>
       </Box>
       <Footer />
@@ -55,10 +58,9 @@ export default function Main() {
   );
 }
 
-const Container = styled.div`
+const MainContainer = styled.div`
   max-width: 600px;
   margin: 0 auto;
-  padding: 0 10px;
   .header {
     padding: 20px 0;
     display: flex;
