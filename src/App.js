5720f760ae9fd64d6e91ae4bcac12dd910493cf2
wangqi <donotlb@gmail.com>
2022-10-14 16:18:29 +0800

    chore: update deps & remove styled-components (#34)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { ThemeProvider as MuiThemeProvider, StyledEngineProvider } from '@mui/material/styles';
-import { ThemeProvider } from 'styled-components';
-import { create } from '@arcblock/ux/lib/Theme';
+import { ThemeProvider } from '@arcblock/ux/lib/Theme';
 import { LocaleProvider } from '@arcblock/ux/lib/Locale/context';
 import { SessionProvider } from './libs/session';
 
@@ -15,23 +13,17 @@ if (window.blocklet && window.blocklet.prefix) {
 } else if (window.env && window.env.apiPrefix) {
   apiPrefix = window.env.apiPrefix;
 }
-const theme = create();
 
 function App() {
-
   return (
-    <StyledEngineProvider injectFirst>
-      <MuiThemeProvider theme={theme}>
-        <ThemeProvider theme={theme}>
-          <LocaleProvider translations={{}}>
-            <SessionProvider serviceHost={apiPrefix}>
-              <CssBaseline />
-              <Main />
-            </SessionProvider>
-          </LocaleProvider>
-        </ThemeProvider>
-      </MuiThemeProvider>
-    </StyledEngineProvider>
+    <ThemeProvider>
+      <LocaleProvider translations={{}}>
+        <SessionProvider serviceHost={apiPrefix}>
+          <CssBaseline />
+          <Main />
+        </SessionProvider>
+      </LocaleProvider>
+    </ThemeProvider>
   );
 }
 
